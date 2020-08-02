642cfc51e1ebad61bc18b2793a066706649629b6
Amelia Brito <ameliagiampaolo@gmail.com>
2020-08-02 14:38:46 -0430

    creando componente peliculas y pruebas con ng g component components/nombreComponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MiComponente } from './components/mi-componente/mi-componente.component'; //nombre de la clase
+import { MiComponente } from './components/mi-componente/mi-componente.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PruebasComponent } from './components/pruebas/pruebas.component'; //nombre de la clase
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponente
+    MiComponente,
+    PeliculasComponent,
+    PruebasComponent
   ],
   imports: [
     BrowserModule
