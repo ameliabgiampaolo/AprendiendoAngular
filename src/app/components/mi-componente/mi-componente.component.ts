5bd4d025782cbe4ebcbfb429723100e6114f1e29
Amelia Brito <ameliagiampaolo@gmail.com>
2020-08-02 13:52:03 -0430

    propiedades de un componente

--- src/app/components/mi-componente/mi-componente.component.ts
+++ src/app/components/mi-componente/mi-componente.component.ts
@@ -3,14 +3,23 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'mi-componente',
     template: ` 
-        <h1> Hola mundo, soy MI COMPONETE </h1>
-        <p>Este es mi primer componente</p>
+        <h1> {{ titulo }} </h1>
+        <p>{{ comentario }}</p>
+        <h2> Estamos en el {{year}} </h2>
     `
 })
 
 export class MiComponente{
 
+    public titulo: string;
+    public comentario: string;
+    public year: number;
+
     constructor(){
+        this.titulo = "Hola mundo, soy MI COMPONETE";
+        this.comentario = "Este es mi primer componente";
+        this.year = 2020;
         console.log("Componente mi-componente cargado !!");
+        console.log(this.titulo, this.comentario, this.year);
     }
 }
